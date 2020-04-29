1e5807044914f75ee4128da5781b2592d4651dd8
Augustus Mayo <gusmayo@gmail.com>
2020-04-29 16:40:05 -0500

    More work on fixing build

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,7 @@ module.exports = {
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
+    libraryExport: 'default',
     libraryTarget: 'commonjs'
   },
   optimization: {
